09ec8f26b875121238c01d59cdfa5fee0c14a629
Dan Tame <dantame@Dans-MacBook-Pro.local>
2014-08-03 12:02:01 +0100

    last minute

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -100,7 +100,7 @@ angular.module('starter.controllers', ['ngCordova'])
           if($scope.duration <= 0) {
             clearInterval(matchInterval);
             mySocket.emit('leave', {c: $scope.currentRoomID});
-            mySocket.emit('data', {m: "gameOver", c:{roomId: $scope.currentRoomId}})
+            mySocket.emit('data', {m: "gameOver", c:{roomId: $scope.currentRoomID}})
             twerkometer.callback = function () {};
             $state.go('app.end');
           }
